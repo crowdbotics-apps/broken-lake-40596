fce68979380aefb4a322095fa0e66e7c96c13846
Crowdbotics <team@crowdbotics.com>
2023-05-10 03:32:54 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "broken-lake-40596",
+  url: "https://broken-lake-40596.botics.co",
+  api: "https://broken-lake-40596.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
